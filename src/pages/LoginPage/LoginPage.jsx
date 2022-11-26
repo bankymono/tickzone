86086dee14c0b1d10abb08d38a3e99a973f349d2
Ayodeji Bankole <Ayodeji.Bankole@Interswitchng.com>
2022-11-26 02:13:55 +0100

    started implementing validation

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,27 +2,58 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../redux/actions/userActions";
+import Swal from 'sweetalert2';
 import "./LoginPage.css";
 import { ClipLoader } from 'react-spinners'
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
+const validationSchema = Yup.object({
+  email:Yup.string().email('Invalid email format').required('required'),
+  password:Yup.string().required('required'),
+})
 
 const LoginPage = ({history}) => {
   const dispatch = useDispatch();
   const userLogin = useSelector(state => state.userLogin);
+
+
 
   const {loading, error, userInfo} = userLogin;
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const formik = useFormik({
+    initialValues:{
+        email:'',
+        password:''
+    },
+    onSubmit: async values => {
+
+        await   dispatch(loginUser(email,password))
+        
+    },
+    validationSchema,
+})
+
 
   useEffect(()=>{
     if(userInfo){
         history.push('/events-dashboard')
     } else if(error) {
-      setEmail('');
-      setPassword('');
-      history.push("/login")
+      Swal.fire({
+        icon: 'error',
+        title: error === "BAD REQUEST"? "Invalid username or password": error,
+        showConfirmButton: false,
+        timer: 2000
+      }).then(()=>{
+
+        setEmail('')
+        setPassword('')
+          history.push('/login')
+      })
+
     }
 },[ userInfo,error,history])
 
@@ -60,7 +91,7 @@ switch(e.target.name){
 
       <div className="login-right-side">
         <div className="login-heading">Sign in for Eventic</div>
-        <form action="" onSubmit={handleSubmit}>
+        <form action="" onSubmit={formik.handleSubmit}>
           <div>
             <div className="login-top-inputs">
              </div>
@@ -68,12 +99,18 @@ switch(e.target.name){
 
             <div className="login-input-wrapper login-other-form">
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id='email' value={email} onChange={handleChange} />
+                <input
+                className={formik.touched.email && formik.errors.email ?"signup-input-error":""}
+                type="email" name="email" id='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <div className="signup-error-msg">{formik.errors.email}</div>
             </div>
 
             <div className="login-input-wrapper login-other-form">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id='password' value={password} onChange={handleChange} />
+                <input
+                className={formik.touched.password && formik.errors.password ?"signup-input-error":""} 
+                type="password" name="password" id='password' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <div className="signup-error-msg">{formik.errors.password}</div>
             </div>
 
           </div>
@@ -82,8 +119,7 @@ switch(e.target.name){
             <div className="login-checkbox-item"><input type="checkbox" /> <div></div>Keep me up to date on latest events.</div>
             <div className="login-checkbox-item"><input type="checkbox" /> <div></div>I accept the  Terms of use  and  subscription</div>
           </div>
-
-          <button className="login-btn">{loading ? <ClipLoader size={12} />: <span>Login</span>}</button>
+          <button type="submit" className="login-btn">{loading ? <ClipLoader size={12} />: <span>Login</span>}</button>
         </form>
       </div>
     </div>
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -6,13 +6,24 @@ import { useSelector } from "react-redux";
 import { registerUser } from "../../redux/actions/userActions";
 import { useEffect } from "react";
 import { ClipLoader } from 'react-spinners';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+
+
+const validationSchema = Yup.object({
+  firstName:Yup.string().required("Field is required"),
+  lastName:Yup.string().required("required"),
+  brandName:Yup.string().required("required"),
+  email:Yup.string().email('Invalid email format').required('required'),
+  password:Yup.string().required('required'),
+})
 
 
 const SignupPage = ({history}) => {  
   const dispatch = useDispatch();
 
   const userRegister = useSelector(state => state.userRegister);
-  const {userSuccess, userLoading, userError} = userRegister
+  const {userSuccess, userLoading, userError} = userRegister;
 
 
   const [firstName, setFirstName] = useState('');
@@ -20,6 +31,30 @@ const SignupPage = ({history}) => {
   const [brandName, setBrandName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const formik = useFormik({
+    initialValues:{
+        firstName:'',
+        lastName:'',
+        brandName:'',
+        email:'',
+        password:''
+    },
+    onSubmit: async values => {
+      const data = {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        brandName: values.brandName,
+        email:values.email,
+        password:values.password
+      }
+
+        await dispatch(registerUser(data))
+        
+    },
+
+    validationSchema,
+})
 
   useEffect(()=>{
     if(userSuccess){
@@ -51,42 +86,42 @@ const SignupPage = ({history}) => {
     userSuccess,history,userError
 ])
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
 
-    const data = {
-      firstName,
-      lastName,
-      brandName,
-      email,
-      password
-    }
+//     const data = {
+//       firstName,
+//       lastName,
+//       brandName,
+//       email,
+//       password
+//     }
     
-    dispatch(registerUser(data))
+//     dispatch(registerUser(data))
                 
-}
+// }
 
-const handleChange = (e) => {
-  switch(e.target.name){
-    case 'firstName':
-      setFirstName(e.target.value)
-      break;
-    case 'lastName':
-      setLastName(e.target.value)
-      break;
-    case 'brandName':
-      setBrandName(e.target.value)
-      break;
-    case 'email':
-      setEmail(e.target.value)
-      break;
-    case 'password':
-      setPassword(e.target.value)
-      break;
-    default:
-      return null;
-  }
-}
+// const handleChange = (e) => {
+//   switch(e.target.name){
+//     case 'firstName':
+//       setFirstName(e.target.value)
+//       break;
+//     case 'lastName':
+//       setLastName(e.target.value)
+//       break;
+//     case 'brandName':
+//       setBrandName(e.target.value)
+//       break;
+//     case 'email':
+//       setEmail(e.target.value)
+//       break;
+//     case 'password':
+//       setPassword(e.target.value)
+//       break;
+//     default:
+//       return null;
+//   }
+// }
 
   return (
     <div className='signup-page-bg'>
@@ -101,33 +136,38 @@ const handleChange = (e) => {
 
       <div className="signup-right-side">
         <div className="signup-heading">Sign up for Eventic</div>
-        <form onSubmit={handleSubmit} action="">
+        <form onSubmit={formik.handleSubmit} action="">
           <div>
             <div className="signup-top-inputs">
               <div className="signup-input-wrapper">
                 <label htmlFor="firstName">First name</label>
-                <input name="firstName" type="text" id='firstName' value={firstName} onChange={handleChange} />
+                <input name="firstName" type="text" id='firstName' className={formik.touched.firstName && formik.errors.firstName ?"signup-input-error":""} value={formik.values.firstName} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <div className="signup-error-msg">{formik.errors.firstName}</div>
               </div>
 
               <div className="signup-input-wrapper">
                 <label htmlFor="lastName">Last name</label>
-                <input type="text" id='lastName' name="lastName" value={lastName} onChange={handleChange} />
+                <input className={formik.touched.lastName && formik.errors.lastName ?"signup-input-error":""} type="text" id='lastName' name="lastName" value={formik.values.lastName} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <div className="signup-error-msg">{formik.errors.lastName}</div>
               </div>
             </div>
 
             <div className="signup-input-wrapper signup-other-form">
                 <label htmlFor="brandName">Brand Name</label>
-                <input type="text" id='brandname' name="brandName" value={brandName} onChange={handleChange} />
+                <input type="text" id='brandname' className={formik.touched.brandName && formik.errors.brandName ?"signup-input-error":""} name="brandName" value={formik.values.brandName} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <div className="signup-error-msg">{formik.errors.brandName}</div>
             </div>
 
             <div className="signup-input-wrapper signup-other-form">
                 <label htmlFor="email">Email</label>
-                <input type="email" id='email' name="email" value={email} onChange={handleChange} />
+                <input type="email" id='email' name="email" className={formik.touched.email && formik.errors.email ?"signup-input-error":""} value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <div className="signup-error-msg">{formik.errors.email}</div>
             </div>
 
             <div className="signup-input-wrapper signup-other-form">
                 <label htmlFor="password">Password</label>
-                <input type="password" id='password' name="password"  value={password} onChange={handleChange}/>
+                <input type="password" id='password' name="password" className={formik.touched.password && formik.errors.password ?"signup-input-error":""} value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+                <div className="signup-error-msg">{formik.errors.password}</div>
             </div>
 
           </div>
@@ -137,7 +177,7 @@ const handleChange = (e) => {
             <div className="signup-checkbox-item"><input type="checkbox" /> <div></div>I accept the  Terms of use  and  subscription</div>
           </div>
 
-          <button className="signup-btn">{userLoading ? <ClipLoader size={12} />:<span>Create your account</span>}</button>
+          <button type="submit" className="signup-btn">{userLoading ? <ClipLoader size={12} />:<span>Create your account</span>}</button>
         </form>
       </div>
     </div>
